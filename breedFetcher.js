ef3d14f6f1f8b62c0cba87afd225490ad16062f5
Nicholas Meisenheimer <souredoutlook@gmail.com>
2021-02-11 20:16:39 +0000

    refactored breedFetcher.js to accept a calback export module to index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,16 @@
 const request = require('request');
-const breedName = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName,(error, response, body) => {
-  if (error) throw error;
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    console.log(`No breed found when searching: "${breedName}"`);
-  } else {
-    console.log(`${data[0].name}: ${data[0].description}`);
-  }
-});
+
+const fetchBreedDescription = function(breedName, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breedName, (error, response, body) => {
+    if (!error) error = null;
+    const data = JSON.parse(body);
+    if (data[0]) {
+      return callback(error, data[0].description);
+    } else {
+      return callback(error, `No breed found when searching: "${breedName}"`);
+    }
+  });
+};
+
+module.exports = {fetchBreedDescription};
